9ef887c38f267bb24a26e2861ec1c868e53b3116
kitty-panics <e222d29f@outlook.com>
2021-09-13 21:43:29 +0800

    feat: 支持生成 ops.json

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# username2uuid
+# USER-NAME to UUID
 
-将用户名转换成 UUID，以方便将 HMCL 中的用户添加进 Minecraft-Server 白名单。
+将用户名转换成 UUID，以方便将 HMCL 中的离线用户添加进 Minecraft-Server 白名单。
 
 ## 使用
 
 1. `git clone --depth=1 git@github.com:kitty-panics/username2uuid.git`
 2. `cd username2uuid/bin`
-3. `bash username2uuid`
+3. 生成配置文件
+    + 生成 **ops.json**
+        + `./username2uuid ops 用户名 > ops.json`
+    + 生成 **whitelist.json**
+        + `./username2uuid wlist 用户名> whitelist.json`
 
-## TODO
-
-同时生成 `ops.json`、`whitelist.json` 白名单配置。
--- src/kitty/panics/Main.java
+++ src/kitty/panics/Main.java
@@ -1,6 +1,6 @@
 package kitty.panics;
 
-import java.util.Scanner;
+import java.util.Objects;
 import java.util.UUID;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -8,12 +8,36 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class Main {
 
     public static void main(String[] args) {
-        // 获取输入
-        System.out.println("=>> Please enter your User-Name:");
-        Scanner input_user_name = new Scanner(System.in);
-        String user_name = input_user_name.nextLine();
+        // 声明变量
+        String JSON_TYPE;
+        String USER_NAME;
+        UUID GEN_UUID;
 
-        // 转换并输出
-        System.out.println(UUID.nameUUIDFromBytes(("OfflinePlayer:" + user_name).getBytes(UTF_8)));
+        // 获取参数 1
+        JSON_TYPE = args[0];
+        // 获取参数 2
+        USER_NAME = args[1];
+
+        // 生成 UUID
+        GEN_UUID = UUID.nameUUIDFromBytes(("OfflinePlayer:" + USER_NAME).getBytes(UTF_8));
+
+        // 判断 JSON_TYPE
+        if(Objects.equals(JSON_TYPE, "ops")) {
+            // 转换并输出
+            System.out.println("[\n"                                    +
+                               "  {\n"                                  +
+                               "    \"uuid\": \"" + GEN_UUID + "\",\n"  +
+                               "    \"name\": \"" + USER_NAME + "\",\n" +
+                               "    \"level\": 4,\n"                    +
+                               "    \"bypassesPlayerLimit\": true\n"    +
+                               "  }\n"                                  +
+                               "]"
+            );
+        } else if(Objects.equals(JSON_TYPE, "wlist")) {
+            // 转换并输出
+            System.out.println(GEN_UUID);
+        } else {
+            System.out.print("=> 参数错误。");
+        }
     }
 }
